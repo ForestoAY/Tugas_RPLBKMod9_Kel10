5460b3ae7bdca7838feba32983b61395ae30dc26
ForestoAY <forestoay@gmail.com>
2023-11-14 13:09:20 +0700

    tes

---
+++ APITugasMod9_Kel10/Data/AccountStore.cs
@@ -0,0 +1,13 @@
+﻿using APITugasMod9_Kel10.Models.Dto;
+
+namespace APITugasMod9_Kel10.Data
+{
+    public static class AccountStore
+    {
+        public static List<AccountDTO> accountList = new List<AccountDTO>
+        {
+             new AccountDTO{Id=1, Username="admin", Password="root"},
+             new AccountDTO{Id=2, Username="abcde", Password="1234"}
+        };
+    }
+}
---
+++ APITugasMod9_Kel10/Models/Dto/AccountDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace APITugasMod9_Kel10.Models.Dto
+{
+    public class AccountDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
